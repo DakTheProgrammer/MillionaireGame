6a52f922706c9e578d11fd4d2bdb318baa232190
Christian Douglas <cedoug13@aol.com>
2020-09-17 14:42:20 -0500

    Hi Dakota

--- millionaire/MainForm.Designer.cs
+++ millionaire/MainForm.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::millionaire.Properties.Resources.who_wants_to_be_a_millionaire_template_1_728;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
